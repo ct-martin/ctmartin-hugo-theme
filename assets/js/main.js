9b106741b18b650b0af79eb9a69455b4b189d30f
Christian Martin <ct-martin@users.noreply.github.com>
2020-04-17 01:14:04 -0400

    Add support for Nuxt templating in layout; lint

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,14 @@
 window.addEventListener('load', () => {
-    document.querySelectorAll('[data-toggle]').forEach(element => {
-        element.addEventListener('click', ()  => {
-            document.querySelector(element.dataset.toggle).classList.toggle('active');
-        });
-    });
-});
-
-window.addEventListener('resize', () => {
-    document.querySelectorAll('[data-toggle]').forEach(element => {
-        document.querySelector(element.dataset.toggle).classList.remove('active');
-    });
-});
+    document.querySelectorAll('[data-toggle]').forEach((element) => {
+      element.addEventListener('click', () => {
+        document.querySelector(element.dataset.toggle).classList.toggle('active')
+      })
+    })
+  })
+  
+  window.addEventListener('resize', () => {
+    document.querySelectorAll('[data-toggle]').forEach((element) => {
+      document.querySelector(element.dataset.toggle).classList.remove('active')
+    })
+  })
+  
