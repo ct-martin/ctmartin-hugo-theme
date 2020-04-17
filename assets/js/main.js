fd46f4103577a57bfa59ae89335ca6eb47e38d85
Christian Martin <ct-martin@users.noreply.github.com>
2020-04-17 01:19:34 -0400

    take 3

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,11 @@
 window.addEventListener('load', () => {
   document.querySelectorAll('[data-toggle]').forEach((element) => {
     element.addEventListener('click', () => {
-    document.querySelector(element.dataset.toggle).classList.toggle('active')
+      document.querySelector(element.dataset.toggle).classList.toggle('active')
     })
   })
 })
-  
+
 window.addEventListener('resize', () => {
   document.querySelectorAll('[data-toggle]').forEach((element) => {
     document.querySelector(element.dataset.toggle).classList.remove('active')
